9fde4aa55cffc96b1e2f63b1dc60738084996170
HienaDev <ant.rodrigues1999@gmail.com>
2026-03-21 21:54:57 +0000

    Add UI

--- Assets/DeathZone.cs
+++ Assets/DeathZone.cs
@@ -36,4 +36,30 @@ public class DeathZone : MonoBehaviour
             Debug.Log($"<color=red>DeathZone:</color> Respawned {other.gameObject.name}");
         }
     }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        // Check if the object's layer is included in our LayerMask
+        if (((1 << collision.gameObject.layer) & detectionLayer) != 0)
+        {
+            // 1. Try to find a Rigidbody
+            Rigidbody rb = collision.gameObject.GetComponent<Rigidbody>();
+            if (rb != null)
+            {
+                // 2. Move and Reset Physics
+                rb.transform.position = respawnPoint.position;
+                rb.transform.rotation = respawnPoint.rotation;
+                // Reset velocities so it doesn't "carry" the fall speed to the new spot
+                rb.linearVelocity = Vector3.zero;
+                rb.angularVelocity = Vector3.zero;
+            }
+            else
+            {
+                // 3. Fallback for non-physics objects
+                collision.transform.position = respawnPoint.position;
+                collision.transform.rotation = respawnPoint.rotation;
+            }
+            Debug.Log($"<color=red>DeathZone:</color> Respawned {collision.gameObject.name}");
+        }
+    }
 }
--- Assets/StrikeManager.cs
+++ Assets/StrikeManager.cs
@@ -5,8 +5,29 @@ public class StrikeManager : MonoBehaviour
     [SerializeField] private int maxStrikes = 3;
     private int currentStrikes = 0;
 
+    [SerializeField] private Material strikeOnMaterial;
+    [SerializeField] private Material strikeOffMaterial;
+    [SerializeField] private Renderer[] strike1;
+    [SerializeField] private Renderer[] strike2;
+    [SerializeField] private Renderer[] strike3;
+
+    private Renderer[][] strikeRenderers;
+
+    private void Start()
+    {
+        strikeRenderers = new Renderer[][] { strike1, strike2, strike3 };
+
+    }
+
     public void AddStrike()
     {
+
+        if (currentStrikes < maxStrikes)
+            foreach (Renderer r in strikeRenderers[currentStrikes])
+            {
+                r.material = strikeOnMaterial;
+            }
+
         currentStrikes++;
         if (currentStrikes >= maxStrikes) LoseGame();
         else print($"Error made! {currentStrikes} out of  {maxStrikes} strikes!");
