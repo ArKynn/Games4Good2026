1be3e1d72f1aa0de065f4fdec2eb4ee733fccc76
HienaDev <ant.rodrigues1999@gmail.com>
2026-03-22 14:39:23 +0000

    Add stuff

--- Assets/StrikeManager.cs
+++ Assets/StrikeManager.cs
@@ -13,32 +13,49 @@ public class StrikeManager : MonoBehaviour
     [SerializeField] private Renderer[] strike3;
 
     private Renderer[][] strikeRenderers;
-    
+
     public UnityEvent onStrike;
 
+    public UnityEvent onGameOver;
     private void Start()
     {
         strikeRenderers = new Renderer[][] { strike1, strike2, strike3 };
 
     }
 
+    private void Update()
+    {
+        // For testing purposes, press S to add a strike
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            AddStrike();
+        }
+    }
+
     public void AddStrike()
     {
 
         if (currentStrikes < maxStrikes)
+
+
             foreach (Renderer r in strikeRenderers[currentStrikes])
             {
                 r.material = strikeOnMaterial;
             }
 
         currentStrikes++;
-        onStrike?.Invoke();
-        if (currentStrikes >= maxStrikes) LoseGame();
-        else print($"Error made! {currentStrikes} out of  {maxStrikes} strikes!");
+        if(currentStrikes <= maxStrikes)
+        {
+            onStrike?.Invoke();
+            if (currentStrikes >= maxStrikes) LoseGame();
+            else print($"Error made! {currentStrikes} out of  {maxStrikes} strikes!");
+        }
+           
     }
 
     private void LoseGame()
     {
+        onGameOver.Invoke();
         print("Game Over! 3 or more errors made!");
     }
 }
